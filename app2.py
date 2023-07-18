8af1e4e3c929efa1bd4c67c552dadce58cdb7eeb
EunAh <eunahp86@gmail.com>
2023-07-18 14:48:35 +0900

    weather

---
+++ app2.py
@@ -0,0 +1,23 @@
+# from flask import Flask, render_template, request, redirect
+# from weather import Weather
+
+# app = Flask(__name__)
+
+# @app.route('/', methods=['GET','POST'])
+# def index():
+#     if request.method=='GET':
+#         weather_result={}
+#         error=0
+#         return render_template('index.html',weather=weather_result,error=error)
+#     elif request.method=='POST':
+#         city_name=request.form['city_name']
+#         if city_name:
+#             weather = Weather(city_name=city_name)
+#             weather_result=weather.get_weather_info()
+#             if weather_result['cod']==200:
+#                 return render_template('index.html', weather=weather_result,)
+        
+        
+
+# # if __name__=='__main__':
+# #     app.run(debug=True, port=3000)
